7421e35238e09e0fb06b6e946cc495f9c1b37df7
Puja <pujak816@gmail.com>
2022-07-30 14:18:14 +0530

    movie app deployment

--- src/App.js
+++ src/App.js
@@ -1,23 +1,82 @@
-import logo from './logo.svg';
-import './App.css';
+import axios from "axios";
+import React, { useEffect, useState } from "react";
+import "./App.css";
+import "./styles.css";
 
 function App() {
+  const [popularMovies, setPopularMovies] = useState([]);
+  const [searchedMovieName, setSearch] = useState("");
+  const [searchedMovieList, setMovieList] = useState([]);
+
+  useEffect(() => {
+    axios
+      .get(
+        "https://api.themoviedb.org/3/movie/popular?api_key=cfe422613b250f702980a3bbf9e90716"
+      )
+      .then((res) => {
+        setPopularMovies(res.data.results);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }, []);
+
+  useEffect(() => {
+    if (searchedMovieName !== "") {
+      axios
+        .get(
+          `https://api.themoviedb.org/3/search/movie?query=${searchedMovieName}&api_key=cfe422613b250f702980a3bbf9e90716`
+        )
+        .then((res) => {
+          setMovieList(res.data.results);
+        })
+        .catch((err) => {
+          console.log(err);
+        });
+    } else {
+      setMovieList([]);
+    }
+  }, [searchedMovieName]);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <form className="search">
+        <input
+          type="search"
+          value={searchedMovieName}
+          placeholder="Search for Movie Title …"
+          onChange={(e) => setSearch(e.target.value)}
+        />
+      </form>
+      <div className="movieWrapper">
+        {searchedMovieList.length === 0 &&
+        searchedMovieName === "" &&
+        popularMovies.length > 0
+          ? popularMovies.map((movie) => (
+              <div className="movieCard">
+                <img
+                  src={`https://image.tmdb.org/t/p/original${movie.poster_path}`}
+                  alt="movie"
+                  className="movieImage"
+                />
+                <figcaption>
+                  <h2 className="movie__title">{movie.original_title}</h2>
+                </figcaption>
+              </div>
+            ))
+          : searchedMovieList.map((movie) => (
+              <div className="movieCard">
+                <img
+                  src={`https://image.tmdb.org/t/p/original${movie.poster_path}`}
+                  alt="movie"
+                  className="movieImage"
+                />
+                <figcaption>
+                  <h2 className="movie__title">{movie.original_title}</h2>
+                </figcaption>
+              </div>
+            ))}
+      </div>
     </div>
   );
 }
